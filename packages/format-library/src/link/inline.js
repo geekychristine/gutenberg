0ded7d2518e54a5b561cdc974e53b34e6c839e35
John Godley <johng75@gmail.com>
2018-11-07 15:00:52 +0000

    Set default link value if undefined
    If you have an anchor with no link and try and set the ‘open in new tab’ it would throw an error. This sets the default to an empty string to prevent the error.

--- packages/format-library/src/link/inline.js
+++ packages/format-library/src/link/inline.js
@@ -159,7 +159,7 @@ class InlineLinkUI extends Component {
 	}
 
 	setLinkTarget( opensInNewWindow ) {
-		const { activeAttributes: { url }, value, onChange } = this.props;
+		const { activeAttributes: { url = '' }, value, onChange } = this.props;
 
 		this.setState( { opensInNewWindow } );
 
